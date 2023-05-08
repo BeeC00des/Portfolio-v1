3f884dee77387ff100a44539a921a6cb9b4e6408
BeeC00des <ajayibcd@gmail.com>
2023-05-08 19:37:44 +0000

    seperated file concerns

---
+++ client/src/components/BigCard.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import Button from "./Button"
+
+const Card = ({ text, year,title,link }) => {
+  return (
+    <div className="card">
+      <div className="cardContent">
+        <span>{year}</span>
+        <p className='cardTitle'>{title}</p>
+        <p className='CardText'>{text}</p>
+        <Button />
+      </div> 
+  </div>
+  )
+}
+
+// Button.defaultProps = {
+//   color: 'steelblue',
+// }
+
+Card.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export default Card;
